9637eb4b41b0672870c5e6241a63cec906bffb03
Harsh <Harsh.Pandya@rblbank.com>
2022-09-10 12:48:45 +0000

    updated pretty

--- ReactApplication/my-react-application/release.config.js
+++ ReactApplication/my-react-application/release.config.js
@@ -1,4 +1,3 @@
-
 /*
 Semantic release config is stored. It provides some default plugins to use
 '@semantic-release/commit-analyzer'->Analyze commit message and will apply convection commit template.
@@ -9,16 +8,17 @@ Adding assets which will be taken from workspace of actions.
 */
 
 module.exports = {
-    branches: "main",
-    repositoryUrl: "https://github.com/codophilic/LearnGitHubActions",
-    plugins: ['@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator', 
-    '@semantic-release/github',
+  branches: "main",
+  repositoryUrl: "https://github.com/codophilic/LearnGitHubActions",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
     {
-        assets: [
-            { path: "build.zip", label: "Build" },
-            { path: "coverage.zip", label: "Coverage Report"}
-        ]
-    }
-]
-}
+      assets: [
+        {path: "build.zip", label: "Build"},
+        {path: "coverage.zip", label: "Coverage Report"},
+      ],
+    },
+  ],
+};
